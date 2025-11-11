1332faa5c474c1f402436c2181e304fb3e51632e
Josh <kapsjosh11@gmail.com>
2025-11-11 15:01:12 -0500

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,29 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio Website",
-  description: "Portfolio Website",
+    title: "Portfolio Website",
+    description: "Portfolio Website",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="h-full w-full">
-      <body
-        className={'antialiased flex flex-col min-h-screen'}
-      >
-        <header className="w-full">
-          {/* <NavBarSelector /> */}
-        </header>
+    return (
+        <html lang="en" className="h-full w-full">
+            <body className={"antialiased flex flex-col min-h-screen"}>
+                <Analytics />
+                <header className="w-full">{/* <NavBarSelector /> */}</header>
 
-        <main className="flex-grow">
-          {children}
-        </main>
+                <main className="grow">{children}</main>
 
-        <footer className="w-full text-center">
-          <span>© 2025 Joshua Kaplan. Code licensed under MIT.</span>
-        </footer>
-      </body>
-    </html>
-  );
+                <footer className="w-full text-center">
+                    <span>© 2025 Joshua Kaplan. Code licensed under MIT.</span>
+                </footer>
+            </body>
+        </html>
+    );
 }
